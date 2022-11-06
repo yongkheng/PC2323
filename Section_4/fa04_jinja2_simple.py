7e8357220a812260dd9517fc8ae6e6eca09953a7
Goh YK <goh.yongkheng@gmail.com>
2022-11-06 14:52:37 +0800

    clean up

--- Section_4/fa04_jinja2_simple.py
+++
@@ -1,3 +0,0 @@
-from jinja2 import Template
-template = Template("Hello {{ name }}! Nice to see you!")
-print(template.render(name="John Smith"))
--- Section_4/fa04_type_hint.py
+++
@@ -1,9 +0,0 @@
-from typing import Union
-
-def square(x : Union[int, float]) -> Union[int, float]:
-    return x*x
-
-print(square(12345))
-print(square(1.2345))
-print(square('12345'))
-
