5555f686a256803874ccaae9c43aa9ea1223efc3
Goh YK <goh.yongkheng@gmail.com>
2022-11-06 15:07:22 +0800

    clean up

---
+++ Section_6/roadmap_12/app_repo/infrastructure/greencity.py
@@ -0,0 +1,16 @@
+from typing import Optional
+import pandas as pd
+
+async def _get_external_report(city: str, factor: Optional[str] = "overall"):
+    df = pd.read_csv("infrastructure/greencities.csv")
+    df.index = df.city.apply(lambda x: x.lower().replace(' ', '_'))
+    resp = {}
+    if city in df.index:
+        resp = {
+            'country': df.loc[city]['Country'],
+            'people': df.loc[city]['People'],
+            'planet': df.loc[city]['Planet'],
+            'profit': df.loc[city]['Profit'],
+            'overall': df.loc[city]['Overall']
+        }
+    return resp
